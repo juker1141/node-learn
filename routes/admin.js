1d99b4fb55aa83dc96fa4ad61fde50d4f604776d
Ryu <ryu@realtime.tw>
2022-11-23 10:30:45 +0800

    update: handlebars template engine

--- routes/admin.js
+++ routes/admin.js
@@ -15,6 +15,9 @@ router.get("/add-product", (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
+    activeProduct: true,
+    formsCSS: true,
+    productCSS: true,
   });
 });
 
